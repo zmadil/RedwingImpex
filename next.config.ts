33456bd76d3c38049662e27dea616c23375f1c1d
Zain <zain@Zains-MacBook-Pro.local>
2026-05-17 19:43:50 -0500

    github workflow actions

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/RedwingImpex",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
